10cbcd79709d913983cf7d2e3c466aba1b684a12
yyq <yyq331505@163.com>
2019-04-21 18:35:40 +0800

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr  8 22:46:34 2019
+
 @author: Samuel
 """
-from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
 import numpy as np
 import pickle
 import six
+import warnings
 from itertools import cycle
 from collections import OrderedDict
 from scipy.sparse import csr_matrix
-from pandas.core.series import Series
-from pandas.core.frame import DataFrame
-import warnings
-
+from sklearn.base import BaseEstimator, TransformerMixin
 
 class DiscreteMinxin(object):
     def get_obj_cols(self, df):
@@ -35,7 +33,7 @@ class OneHotEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
     
     def __init__(self, dummy_na=True, handle_unknown='ignore',
                  category_threshold=50, drop_threshold_cols=True,
-                 replace_na='null'):
+                 replace_na=-99):
         """
         parameter
         ---------
@@ -67,7 +65,7 @@ class OneHotEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
            not be used, otherwise all object columns and extra_numeric_cols 
            will be encoded.
         """
-        if not isinstance(X, DataFrame): 
+        if not isinstance(X, pd.DataFrame): 
             raise TypeError('X should be DataFrame object')
         
         if y is not None:
@@ -77,56 +75,50 @@ class OneHotEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         else:
             self._dim = X.shape[1]
             
-        # get encoder columns 
         if cols_to_encoder is None:
             cols = self.get_obj_cols(X)
-            if extra_numeric_cols is not None:
-                cols += list(extra_numeric_cols)
+            cols += list(extra_numeric_cols) if extra_numeric_cols is not None \
+                    else []
         else:
             cols = cols_to_encoder   
             
         cols = list(set(cols))
-        if len(cols)==0:
-            print('no colums to encoder')
-            return
         
-        if y in cols:
+        #'no colums to encoder'
+        if len(cols)==0:
+            return  
+        # remove target if it is included in data source
+        if isinstance(y, six.string_types) and y  in cols:
             cols.remove(y)
             
         # re-order cols by original order 
         cols = sorted(cols, key=X.columns.get_loc)       
-        # convert na to nullsym for sake of simplicity
-        df = X[cols].fillna(self.replace_na)
-        
+        # convert na to pre -defined vlaue 
+        df = X[cols].fillna(self.replace_na, downcast='infer')
         # generato rules 
         cats_list = pd.Series()
         for col in cols:
-            cats_list[col] = df[col].unique().tolist()
-            if (not self.dummy_na) and (self.replace_na in cats_list[col]):
-                cats_list[col].remove(self.replace_na) 
-        print(cats_list)      
+            cats_list[col] = df[col].unique().tolist() 
+            if not self.dummy_na and self.replace_na in cats_list[col]:
+                cats_list[col].remove(self.replace_na)
+                
         cats_cnt = cats_list.apply(lambda x: len(x))
         # exclude columns of too manay categories or just one category
         drop_mask = (cats_cnt > self.category_threshold) | (cats_cnt==1)
         drop_index = cats_cnt[drop_mask].index
         cats_list = cats_list[~cats_list.index.isin(drop_index)]
-#        if self.sort_value:
-#            cats_list = cats_list.apply(lambda x: sorted(x))
-        
+       
         self.drop_cols = drop_index.tolist()
         self.encode_cols = cats_list.index.tolist()
         
         maps={}
         for col in self.encode_cols:
             # map each val in col into a index
-            vallist = cats_list[col]   
-            validx = OrderedDict({str(val):i for i,val in enumerate(vallist)})
-            maps[col] = validx
+            val_list = cats_list[col]   
+            val_map = OrderedDict({val:i for i,val in enumerate(val_list)})
+            maps[col] = val_map
             
         self.mapping = maps
-        
-        # convert back to na 
-        #X[cols].replace({self.replace_na:np.nan},inplace=True)
         
         
     def transform(self, X, y=None, dtype=None, inplace=False): 
@@ -136,7 +128,7 @@ class OneHotEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         dtype: specifies the dtype of encoded value
         """
         
-        if not isinstance(X, DataFrame):
+        if not isinstance(X, pd.DataFrame):
             raise TypeError('X shoule be DataFrame object')
         if y is not None:
             if y not in X.columns:
@@ -150,9 +142,10 @@ class OneHotEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
             X = X.copy() # X=X.copy(deep=True)
             
         if self.drop_threshold_cols:
-            X.drop(self.drop_cols,axis=1, inplace=True)
+            X.drop(self.drop_cols, axis=1, inplace=True)
             
-        data_to_encode = X[self.encode_cols].fillna(self.replace_na)
+        data_to_encode = X[self.encode_cols].fillna(self.replace_na,
+                          downcast='infer')
         with_dummies = [X.drop(self.encode_cols,axis=1)]
         
         prefix = self.encode_cols
@@ -178,7 +171,7 @@ class OneHotEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         dummy_cols = [dummy_str.format(prefix=prefix,sep=prefix_sep,val=str(v))
                       for dummy_str, v in zip(dummy_strs, maps.keys())]
         
-        if isinstance(data, Series):
+        if isinstance(data, pd.Series):
             index = data.index
         else:
             index = None
@@ -186,7 +179,7 @@ class OneHotEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         row_idxs= []
         col_idxs= []
         for i, v in enumerate(data):
-            idx = maps.get(str(v),None)
+            idx = maps.get(v,None)
             if idx is None:
                 print("{} only exist in test column '{}'".format(v, prefix))
             else:
@@ -198,7 +191,7 @@ class OneHotEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         out = pd.SparseDataFrame(sarr, index=index, columns=dummy_cols,
                            default_fill_value=0,dtype=dtype)
         
-        return out
+        return out.astype(dtype)
         
     
     
@@ -228,7 +221,7 @@ class MeanEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         
     def fit(self, X, y, cols_to_encode=None, extra_numeric_cols=None):
         
-        if not isinstance(X, DataFrame):
+        if not isinstance(X, pd.DataFrame):
             raise ValueError('X should be DataFrame type')
         
         if isinstance(y, six.string_types):
@@ -247,22 +240,20 @@ class MeanEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         # get encoder columns 
         if cols_to_encode is None:
             cols = self.get_obj_cols(X)
-            if extra_numeric_cols is not None:
-                cols += list(extra_numeric_cols)
+            cols += list(extra_numeric_cols) if extra_numeric_cols is not None\
+                    else []
         else:
             cols = cols_to_encode    
         cols = list(set(cols))
         
         if len(cols)==0:
-            print('no colums to encoder')
             return     
-
         # re-order cols by original order 
         cols = sorted(cols, key=X.columns.get_loc)
         self.encode_cols = cols
         
         data_to_encode = X[self.encode_cols+['_y_']]
-        # convert na to a predefined value 
+        # convert na to a pre-defined value 
         data_to_encode.fillna(self.replace_na, downcast='infer',inplace=True)
         prior = data_to_encode['_y_'].value_counts()/len(data_to_encode['_y_'])
         prior.sort_index(axis=0,inplace=True)
@@ -292,7 +283,7 @@ class MeanEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         self.mapping = maps
             
     def transform(self, X, y=None):
-        if not isinstance(X, DataFrame):
+        if not isinstance(X, pd.DataFrame):
             raise ValueError('X should be DataFrame type')
         if isinstance(y, six.string_types) and y in X.columns:
             if  self._dim != X.shape[1] -1:
@@ -329,7 +320,7 @@ class MeanEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         dummy_cols = [dummy_str.format(prefix=prefix,sep=prefix_sep,val=str(v))
                       for dummy_str, v in zip(dummy_strs, maps.columns)]
         
-        if isinstance(data, Series):
+        if isinstance(data, pd.Series):
             index = data.index
         else:
             index = None
@@ -343,7 +334,7 @@ class MeanEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
                 raise ValueError(msg)
             else:
                 print(msg)
-                enc_df.fillna(self.prior,inplace=True)  
+                enc_df.fillna(self.prior, inplace=True)  
                 
         enc_df.index = index
         enc_df.columns = dummy_cols
@@ -352,16 +343,6 @@ class MeanEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
             
         return enc_df
         
-    def _fill_na(self, df, cols):
-        
-        dtypes = df.dtypes
-        for col in cols:
-            if any(df[col].isna()) :
-                df.loc[:, col] = df[col].fillna(self.replace_na)
-                if dtypes[col].name.startswith('float'):
-                    df.loc[:,col] = df[col].astype(int)
-                elif dtypes[col].name == 'object':
-                    df.loc[:,col] = df[col].astype(str)
 
 class WoeEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
     """
@@ -370,7 +351,8 @@ class WoeEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
     def __init__(self, dummy_na = True, handle_unknown='zero', replace_na=-99,
                  reg = 1):
         '''
-        dummy_na bool, force to True
+        dummy_na: bool, if true null value is treated as a category, otherwise
+                  null value will be filled with zero.              
         handle_unknown: one of ('zero', 'error')
         reg: int, bayesian prior value  to avoid divding by zero when calculate woe.
         
@@ -384,7 +366,7 @@ class WoeEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         
     def fit(self, X, y, cols_to_encode = None, extra_numeric_cols=None):
         
-        if not isinstance(X, DataFrame):
+        if not isinstance(X, pd.DataFrame):
             raise ValueError('X should be DataFrame type')
         
         if isinstance(y, six.string_types):
@@ -403,15 +385,14 @@ class WoeEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         # get encoder columns 
         if cols_to_encode is None:
             cols = self.get_obj_cols(X)
-            if extra_numeric_cols is not None:
-                cols += list(extra_numeric_cols)
+            cols += list(extra_numeric_cols) if extra_numeric_cols is not None \
+                    else []
         else:
             cols = cols_to_encode    
         # re-order cols by original order 
         self.encode_cols = sorted(list(set(cols)), key=X.columns.get_loc)
         
         if len(self.encode_cols)==0:
-            print('no colums to encoder')
             return     
 
         data_to_encode = X[self.encode_cols+['_y_']]
@@ -431,14 +412,15 @@ class WoeEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
     def _compute_woe(self, df, var, y='_y_'):
         grp = df[y].groupby(df[var]).agg(['sum',lambda x: x.count()-x.sum()])
         grp = grp.rename(columns={'sum':'pos', '<lambda>':'neg'})
-        
-        #bayesian prior to avoid divide by zero
+        #use bayesian prior value to avoid dividing by zero
         woe = np.log((grp['pos']+self.reg)/(grp['neg']+self.reg)) - \
-              np.log((self._pos+2*self.reg)/(self._neg+2*self.reg))
+              np.log((self._pos+2*self.reg)/(self._neg+2*self.reg))             
+        if not self.dummy_na and self.replace_na in woe:
+            woe[self.replace_na] = 0.0          
         return woe
     
     def transform(self, X, y=None, inplace=False):
-        if not isinstance(X, DataFrame):
+        if not isinstance(X, pd.DataFrame):
             raise ValueError('X should be DataFrame type')
         if isinstance(y, six.string_types) and y in X.columns:
             if  self._dim != X.shape[1] -1:
@@ -454,10 +436,9 @@ class WoeEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
         
         X[self.encode_cols] = X[self.encode_cols].fillna(self.replace_na,
                                      downcast = 'infer')
-        print(X)
         msg = "unseen category {} in column '{}'"
         for col in self.encode_cols:
-            X[col] = X[col].map(self.mapping[col])
+            X[col] = X[col].map(self.mapping[col]) # unseen value filled with NaN
             #handle unknown value
             if any(X[col].isnull()):
                 if self.handle_unknown == 'error':
@@ -469,15 +450,17 @@ class WoeEncoder(BaseEstimator, TransformerMixin, DiscreteMinxin):
                
 if __name__== '__main__':
     warnings.filterwarnings('ignore')
+    
+    print('OneHotEncoder'.center(40,'-'))
     df = pd.DataFrame([[1,2,3,'a',2.0],[1,np.nan,4,'6',3.0],[2,3,4,5,6],
                        [2.0,3,4,5,np.nan]],columns=['x','y','z','j','k'])
     df.index=['1','2','3','4']
-    #df = pd.DataFrame([[1,2,3],[3,4,5]])
-#    ohe = OneHotEncoder(dummy_na=False)
-#    ohe.fit(df,extra_numeric_cols=['y','x','j'])
-#    ret = ohe.transform(df)
-#    print(df)
-#    print(ret)
+    ohe = OneHotEncoder(dummy_na=False)
+    ohe.fit(df,extra_numeric_cols=['y','x','j'])
+    print(df)
+    print(ohe.transform(df))
+    
+    print('MeanEncoder'.center(40,'-'))
     df = pd.DataFrame([[1,2,3],[np.nan,3,4],[2,3,6],[2,4,2]],columns=['a','b','c'])
     y = pd.Series([1,0,0,1])
     
@@ -491,3 +474,4 @@ if __name__== '__main__':
     wec = WoeEncoder()
     wec.fit(df,y, extra_numeric_cols=['a','b','c'])
     print(wec.transform(df))
+    
